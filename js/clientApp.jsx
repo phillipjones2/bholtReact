f5cd20ffd3c3dceab6897d9fc57df4a3e88a2173
Phillip Jones <phillipemersonjones@gmail.com>
2016-08-22 14:17:40 -0500

    moving along on the workshop

--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -6,7 +6,7 @@ var App = () => (
     <div className='home-info'>
       <h1 className='title'>svideo</h1>
       <input className='search' type='text' placeholder='Search' />
-      <button className='browse-all'> or Browse All </button>
+      <button className='browse-all'> or Browse All!!!! </button>
     </div>
   </div>
 )

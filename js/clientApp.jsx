db245016c1524736ca9f85467bbac3bfdcd79b89
Phillip Jones <phillipemersonjones@gmail.com>
2016-08-23 08:31:01 -0500

    add search page and router

--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -1,11 +1,13 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./landing')
+const Search = require('./search')
 const { Router, Route, hashHistory } = require('react-router')
 
 const App = () => (
   <Router history={hashHistory}>
     <Route path='/' component={Landing} />
+    <Route path='/search' component={Search} />
   </Router>
 )
 

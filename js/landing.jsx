6a8cb3acf9379455f05bf858b1ec966f26e6e467
Phillip Jones <phillipemersonjones@gmail.com>
2016-08-24 14:03:14 -0500

    search works on the landing page

--- js/landing.jsx
+++ js/landing.jsx
@@ -1,12 +1,46 @@
 const React = require('react')
+const { hashHistory } = require('react-router')
 const { Link } = require('react-router')
+const { connector } = require('./store')
 
-const Landing = () => (
-  <div className='home-info'>
-    <h1 className='title'>svideo</h1>
-    <input className='search' type='text' placeholder='Search' />
-    <Link to='/search' className='browse-all'> or Browse All!!!! </Link>
-  </div>
-)
+class Landing extends React.Component {
+  constructor (props) {
+    super(props)
 
-module.exports = Landing
+    this.handleSearchTermEvent = this.handleSearchTermEvent.bind(this)
+    this.gotoSearch = this.gotoSearch.bind(this)
+  }
+  handleSearchTermEvent (e) {
+    this.props.setSearchTerm(e.target.value)
+  }
+  gotoSearch (e) {
+    hashHistory.push('search')
+    e.preventDefault()
+  }
+  render () {
+    return (
+      <div className='home-info'>
+        <h1 className='title'>svideo</h1>
+        <form onSubmit={this.gotoSearch}>
+          <input
+            value={this.props.searchTerm}
+            onChange={this.handleSearchTermEvent}
+            className='search'
+            type='text'
+            placeholder='Search'
+          />
+        </form>
+        <Link to='/search' className='browse-all'> or Browse All!!!! </Link>
+      </div>
+    )
+  }
+}
+
+const { func, string } = React.PropTypes
+
+Landing.propTypes = {
+  searchTerm: string,
+  setSearchTerm: func
+}
+
+module.exports = connector(Landing)

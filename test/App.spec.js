6781f6664c3982d418f25b61255cb09d08aa30cf
Phillip Jones <phillipemersonjones@gmail.com>
2016-08-24 08:18:59 -0500

    workshop testing

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,9 +1,30 @@
 /* eslint-env mocha */
 
 const { expect } = require('chai')
+const React = require('react')
+const Search = require('../js/search')
+const ShowCard = require('../js/showCard')
+const { shallow, mount } = require('enzyme')
+const { shows } = require('../public/data')
 
-xdescribe('<Search />', () => {
-  it('should pass', () => {
-    expect(1 + 1).to.equal(5)
+describe('<Search />', () => {
+  it('should render the brand', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.contains(<h1 className='brand'>svideo</h1>)).to.be.true
+  })
+
+  it('should render as many shows as there are data', () => {
+    const wrapper = shallow(<Search />)
+    console.log(wrapper.debug())
+    expect(wrapper.find(ShowCard).length).to.equal(shows.length)
+  })
+
+  it('should filter correctly given new state', () => {
+    const wrapper = mount(<Search />)
+    const input = wrapper.find('.search-input')
+    input.node.value = 'house'
+    input.simulate('change')
+    expect(wrapper.state('searchTerm')).to.equal('house')
+    expect(wrapper.find('.show-card').length).to.equal(2)
   })
 })
